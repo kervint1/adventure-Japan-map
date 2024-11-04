d2508c9fda8811e6203fe8f8983b37bdafbc2146
kervint1 <s1f102302124@iniad.org>
2024-11-04 11:15:56 +0900

    test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders animap text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/animap/i);
   expect(linkElement).toBeInTheDocument();
 });
