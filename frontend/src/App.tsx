e28c1af2a5e6101dc7afc6c345121a787ae69d2f
kervint1 <s1f102302124@iniad.org>
2024-11-04 11:06:32 +0900

    Add frontend files to version control

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>animap</div>
+      <div className='content'>map</div>
+      <div className='footer'>iniad</div>
+    </div>
+  );
+}
+
+export default App;
