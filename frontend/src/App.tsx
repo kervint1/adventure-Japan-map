63b0c234617c02ea6beb166d537ecbeeecd5d1e8
kervint1 <s1f102302124@iniad.org>
2024-11-07 04:07:28 +0900

    ディレクトリ構成2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AdventureSpotList from './AdventureSpotList';
+// import AdventureSpotList from './AdventureSpotList';
 import Home from './components/pages/home';
 
 const App: React.FC = () => {
