03bece1340a4cf93ff849584f54ecfef31d623bd
1F10230250 <s1f102302506@iniad.org>
2024-11-07 05:38:52 +0900

    header(仮)

--- frontend/src/components/UI/header.tsx
+++ frontend/src/components/UI/header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../../css/header.css';
 
 const header = () => {
   return (
     <div>
       <div className='Header'>
-        <h1>HOME</h1>
+        <h1>AJM</h1>
         <div className='mene'></div>
       </div>
       <div className='Map'></div>
