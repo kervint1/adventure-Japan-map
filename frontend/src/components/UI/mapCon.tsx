536431b987a9c72bd590e598e5f6d8445c4011e9
kervint1 <s1f102302124@iniad.org>
2024-11-08 14:05:26 +0900

    現在位置機能

--- frontend/src/components/UI/mapCon.tsx
+++ frontend/src/components/UI/mapCon.tsx
@@ -26,37 +26,41 @@ const places: Place[] = [
 
 const MapCon = () => {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY!,
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY!,
   }) ;
 
   const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
+  const [userLocation, setUserLocation] = useState<{ lat: number; lng: number } | null>(null);
 
   const onMapClick = useCallback((place: Place) => {
     setSelectedPlace(place);
   }, []);
 
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        () => {
+          console.log("ユーザーの位置情報を取得できませんでした。");
+        }
+      )
+    }
+  }, []);
+
   if ( !isLoaded ) return <div>Loading...</div>
 
-  // const [userLocation, setUserLocation] = useState<{ lat: number; lng: number } | null>(null);
-
-  // useEffect(() => {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(
-  //       (position) => {
-  //         setUserLocation({
-  //           lat: position.coords.latitude,
-  //           lng: position.coords.longitude,
-  //         });
-  //       },
-  //       () => {
-  //         console.log("ユーザーの位置情報を取得できませんでした。");
-  //       }
-  //     )
-  //   }
-  // }, []);
   return (
     <div>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+      <GoogleMap mapContainerStyle={containerStyle} center={userLocation || center} zoom={15}>
+        {userLocation && (
+          <Marker position={userLocation} icon="http://maps.google.com/mapfiles/ms/icons/blue-dot.png" />
+        )},
         {places.map((place) => (
           <Marker 
             key={place.id}
